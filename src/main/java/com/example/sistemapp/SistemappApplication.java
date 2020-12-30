f5591633a3f6c74edeac09efe670a29939a67b46
s.santos.pimenta <simoneperiodista3@gmail.com>
2020-12-30 08:33:53 -0300

    First commit

---
+++ src/main/java/com/example/sistemapp/SistemappApplication.java
@@ -0,0 +1,13 @@
+package com.example.sistemapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemappApplication.class, args);
+	}
+
+}
