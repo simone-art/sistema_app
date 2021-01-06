918e9e5c850f323fda4bbfaccec9ef684f4224b4
s.santos.pimenta <simoneperiodista3@gmail.com>
2021-01-06 15:13:18 -0300

    "Created delete button and delete method. Close #9"

--- src/main/java/com/example/sistemapp/controller/UsuarioController.java
+++ src/main/java/com/example/sistemapp/controller/UsuarioController.java
@@ -4,9 +4,12 @@ import com.example.sistemapp.entity.Usuario;
 import com.example.sistemapp.repository.UsuarioRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import javax.validation.Valid;
 
 
 @Controller
@@ -15,18 +18,33 @@ public class UsuarioController {
     @Autowired
     private UsuarioRepository ur;
 
-    @RequestMapping(value="/cadastrarUsuario", method= RequestMethod.GET)
-    public String form(){
+    @RequestMapping(value = "/cadastrarUsuario", method = RequestMethod.GET)
+    public String form() {
         return "usuario/formUsuario";
     }
 
 
-    @RequestMapping(value="/cadastrarUsuario", method= RequestMethod.POST)
-    public String form(Usuario usuario){
-        //Salva os dados no banco de dados
-        ur.save(usuario);
-        return "redirect:/cadastrarUsuario";
+    @RequestMapping(value = "/cadastrarUsuario", method = RequestMethod.POST)
+    public String form(@Valid Usuario usuario) {
+         ur.save(usuario);
+         return "redirect:/cadastrarUsuario";
     }
+
+
+//   @RequestMapping(value = "/cadastrarUsuario", method = RequestMethod.POST)
+//    public String form(@Valid Usuario usuario, BindingResult bindingResul, RedirectAttributes attributes) {
+//        if (bindingResul.hasErrors()) {
+//            attributes.addFlashAttribute("mensagem", "Verifique os campos!");
+//            return "redirect:/cadastrarUsuario";
+//        }
+//        else {
+//            ur.save(usuario);
+//            attributes.addFlashAttribute("mensagem", "Cadastro feito com sucesso!");
+//            return "redirect:/conteudo";
+//
+//        }
+//    }
+
 
     @RequestMapping("/usuarios")
     public ModelAndView ListaUsuarios(){
@@ -35,6 +53,42 @@ public class UsuarioController {
         mv.addObject("usuarios", Usuarios);
         return mv;
     }
+
+    @GetMapping("/deletar/{codigo}")
+    public String deletarUsuario(@PathVariable long codigo){
+        Usuario usuario = ur.findByCodigo(codigo);
+        ur.delete(usuario);
+        return "redirect:/usuarios";
+    }
+
+
+    // Cadastro Usuario edit
+//    @RequestMapping(value = "/usuarios", method = RequestMethod.GET)
+//    public ModelAndView editarUsuario(long codigo) {
+//        Usuario usuario = ur.findByCodigo(codigo);
+//        ModelAndView mv = new ModelAndView("usuario/index");
+//        mv.addObject("usuario", usuario);
+//        return mv;
+//    }
+
+//    @RequestMapping(value = "{codigo}", method = RequestMethod.DELETE)
+//    public String deletarUsuario(@PathVariable long codigo, Usuario usuario, RedirectAttributes attributes) {
+//        ur.delete(usuario);
+//        attributes.addFlashAttribute("success", "Usuário deletado com sucesso!");
+//        return "redirect:/cadastrarUsuario";
+//    }
+
+
+//    @RequestMapping ("/usuarios/{id}")
+//    public String deletarUsuario( long codigo){
+//        Usuario usuario = ur.findByCodigo(codigo);
+//        ur.delete(usuario);
+//        return "redirect:/cadastrarUsuario";
+//    }
+//
+
+
+
 }
 
 
